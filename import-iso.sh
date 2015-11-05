7c1a64c12ac1b1cfbe1b6965ea839a7f3ab4351c
guoshaowei <guoshaowei@rd.tuan800.com>
2015-11-05 15:36:39 +0800

    add

--- import-iso.sh
+++ import-iso.sh
@@ -4,7 +4,7 @@ mountpoint=/mnt
 
 #Import isos to cobbler
 for iso in `ls /opt/iso`; do
-        ISO_NAME=$($iso | cut -d"." -f1)
+    ISO_NAME=${iso%.*}
     mkdir -p $ISO_NAME
 	mount -t iso9660 -o loop,ro /opt/iso/$iso $mountpoint/$ISO_NAME
 done
